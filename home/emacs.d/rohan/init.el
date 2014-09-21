9e90fb17b36d86fbe2ef13b4edf931b77e064230
Rohan McGovern <rohan@mcgovern.id.au>
2014-09-21 14:00:13 +1000

    emacs: install packages

--- home/emacs.d/rohan/init.el
+++ home/emacs.d/rohan/init.el
@@ -1,3 +1,16 @@
 (require 'package)
 (package-initialize)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(defun rohan--ensure-installed (sym)
+  (if (not (package-installed-p sym))
+      (package-install sym)))
+
+(if (and (boundp 'rohan--init-packages) rohan--init-packages)
+    (progn
+      (package-list-packages)
+      (rohan--ensure-installed 'helm)
+      (rohan--ensure-installed 'cider)))
+
+(helm-mode)
+(recentf-mode)
