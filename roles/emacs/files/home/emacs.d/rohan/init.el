bd5293c16d220ee15390e837ae01001b7c213d0e
Rohan McGovern <rohan@mcgovern.id.au>
2014-10-23 13:32:45 +1000

    emacs: use melpa stable, improve cider

--- roles/emacs/files/home/emacs.d/rohan/init.el
+++ roles/emacs/files/home/emacs.d/rohan/init.el
@@ -1,6 +1,7 @@
 (require 'package)
 (package-initialize)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+  '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
 
 (defun rohan--ensure-installed (sym)
   (if (not (package-installed-p sym))
@@ -10,10 +11,14 @@
     (progn
       (package-list-packages)
       (rohan--ensure-installed 'helm)
-      (rohan--ensure-installed 'helm-git-grep)
+      ; this is not in melpa-stable?
+      ;(rohan--ensure-installed 'helm-git-grep)
       (rohan--ensure-installed 'cider)
       (rohan--ensure-installed 'projectile)
-      (rohan--ensure-installed 'yaml-mode)))
+      (rohan--ensure-installed 'paredit)
+      (rohan--ensure-installed 'company)
+      ;(rohan--ensure-installed 'yaml-mode)
+      ))
 
 (helm-mode)
 (recentf-mode)
@@ -21,3 +26,10 @@
 
 ; key bindings
 (global-set-key (kbd "C-c g") 'helm-git-grep)
+
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+(add-hook 'cider-mode-hook 'cider-eval-ns-form)
+(add-hook 'cider-mode-hook 'paredit-mode)
+(add-hook 'cider-mode-hook 'company-mode)
+(setq cider-auto-select-error-buffer nil)
+;(setq cider-stacktrace-default-filters '(java))
